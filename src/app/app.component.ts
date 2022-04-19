d1c1d80990199c3dca25048a6307ab30adf0b7f6
Manuel Hernández <carlosmanuel.hernandez@bosonit.com>
2022-04-19 15:51:03 +0200

    Feat: Basic working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DisplayHide';
+  title: string = 'DisplayHide';
+  display: Boolean = true;
+
+  displayHide(){
+    this.display= !this.display;
+  }
 }
